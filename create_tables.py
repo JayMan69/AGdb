7cc6d2c95957fcf2d2f9ccc2123b83105382a41f
Jaison <manianj@gmail.com>
2018-06-13 16:44:45 -0500

    first lambda landler

--- create_tables.py
+++ create_tables.py
@@ -52,7 +52,11 @@ class Stream_Details(Base):
     id = Column(Integer, primary_key=True)
     stream_id = Column(Integer, ForeignKey('Stream.id'))
     stream = relationship(Stream)
-    manifest_file_name = Column(String(250), nullable=False)
+    # manifest_file_name can be filled in later
+    manifest_file_name = Column(String(250), nullable=True)
+    # True / False
+    live = Column(String(10), nullable=True)
+    # 640x320x3
     resolution = Column(String(250), nullable=True)
     start_time = Column(DateTime, nullable=True)
     end_time = Column(DateTime, nullable=True)
@@ -71,11 +75,12 @@ class Stream_MetaData(Base):
 
 
 
+if __name__ == '__main__':
+    print('In main of create_tables')
+    connection_string = "mysql://"+rds_config.db_username+':'+rds_config.db_password+ '@' +\
+                        rds_config.db_endpoint+':'+ str(rds_config.db_port) +'/' + rds_config.db_name
+    engine = create_engine(connection_string)
 
-connection_string = "mysql://"+rds_config.db_username+':'+rds_config.db_password+ '@' +\
-                    rds_config.db_endpoint+':'+ str(rds_config.db_port) +'/' + rds_config.db_name
-engine = create_engine(connection_string)
-
-# Create all tables in the engine. This is equivalent to "Create Table"
-# statements in raw SQL.
-#Base.metadata.create_all(engine)
+    # Create all tables in the engine. This is equivalent to "Create Table"
+    # statements in raw SQL.
+    Base.metadata.create_all(engine)
