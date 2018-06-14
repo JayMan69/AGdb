43889f64bb14d4520b5bfdf807d14dccf761ae8e
Jaison <manianj@gmail.com>
2018-06-13 20:18:15 -0500

    initial multiple paths

--- database.py
+++ database.py
@@ -2,21 +2,78 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import rds_config
-from create_tables import  Client
+from create_tables import Client,Camera
+import json
 
-Base = declarative_base()
+class database:
+    def __init__(self,id=None):
+        Base = declarative_base()
 
-connection_string = "mysql://"+rds_config.db_username+':'+rds_config.db_password+ '@' +\
-                    rds_config.db_endpoint+':'+ str(rds_config.db_port) +'/' + rds_config.db_name
+        connection_string = "mysql://"+rds_config.db_username+':'+rds_config.db_password+ '@' +\
+                            rds_config.db_endpoint+':'+ str(rds_config.db_port) +'/' + rds_config.db_name
 
-engine = create_engine(connection_string)
-Base.metadata.bind = engine
+        engine = create_engine(connection_string)
+        Base.metadata.bind = engine
 
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+        DBSession = sessionmaker(bind=engine)
+        self.session = DBSession()
+        if id != None:
+            self.id = id
 
-# Insert a Person in the person table
-new_person = Client(name='Armour Grid')
-session.add(new_person)
-session.commit()
+    def get_clients(self):
+        # List clients in the client table
+        count = 0
+        rs = []
+        for instance in self.session.query(Client).order_by(Client.id):
+            count = count + 1
+            rs.append({instance.id:instance.name})
+        rs1 = {'count':count,'result_set':rs}
+        return rs1
 
+    def get_client(self):
+        # List clients in the client table
+        count = 0
+        rs = []
+        for instance in self.session.query(Client).filter(Client.id== self.id):
+            count = count + 1
+            rs.append({instance.id:instance.name})
+        rs1 = {'count':count,'result_set':rs}
+        return rs1
+
+    def get_cameras(self):
+        # List clients in the client table
+        count = 0
+        rs = []
+        for instance in self.session.query(Camera).order_by(Camera.id):
+            count = count + 1
+            rs.append({instance.id:instance.name})
+        rs1 = {'count':count,'result_set':rs}
+        return rs1
+
+    def get_camera(self):
+        # List clients in the client table
+        count = 0
+        rs = []
+        for instance in self.session.query(Camera).filter(Camera.id== self.id):
+            count = count + 1
+            rs.append({instance.id:instance.name})
+        rs1 = {'count':count,'result_set':rs}
+        return rs1
+
+
+
+def testHarness():
+    event = {}
+    #event['camera_id'] = 10
+
+    if 'camera_id' in event:
+        db = database(event['camera_id'])
+        body = db.get_camera()
+    else:
+        db = database()
+        body = db.get_cameras()
+
+    print(json.dumps(body))
+
+if __name__ == '__main__':
+    testHarness()
--- handler.py
+++ handler.py
@@ -1,45 +1,37 @@
 import json
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-import rds_config
-from create_tables import  Client
+import database
 
+def clients(event, context):
 
-def hello(event, context):
-    body = {
-        "message": "Test1",
-        "input": event
-    }
+    if 'client_id' in event:
+        db = database(event['client_id'])
+        body = db.get_client()
+    else:
+        db = database()
+        body = db.get_clients()
 
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
 
-    Base = declarative_base()
-
-    connection_string = "mysql://" + rds_config.db_username + ':' + rds_config.db_password + '@' + \
-                        rds_config.db_endpoint + ':' + str(rds_config.db_port) + '/' + rds_config.db_name
-
-    engine = create_engine(connection_string)
-    Base.metadata.bind = engine
-
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-
-    # Insert a Person in the person table
-    new_person = Client(name='Armour Grid3')
-    session.add(new_person)
-    session.commit()
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
 
+def cameras(event, context):
+
+    if 'camera_id' in event:
+        db = database(event['client_id'])
+        body = db.get_camera()
+    else:
+        db = database()
+        body = db.get_cameras()
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+
+    return response
