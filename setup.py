440ebd84493d8e85979682977e97d526d3c77711
Jaison <manianj@gmail.com>
2018-06-20 07:41:44 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='AGdb',
+      version='0.1',
+      description='Access to Analytics Database plus Lambdas',
+      url='http://github.com/JayMan69/AGdb',
+      author='Jaison Manian',
+      author_email='jaison@armourgrid.com',
+      license='MIT',
+      packages=['AGdb'],
+      zip_safe=False)
