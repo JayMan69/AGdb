e3f6c4ff4ba18089fac8de91ffd71e8470fa425e
Jaison <manianj@gmail.com>
2018-06-20 10:56:40 -0500

    added checking for header in handler

--- handler.py
+++ handler.py
@@ -4,12 +4,21 @@ from database import database
 
 def clients(event, context):
 
-    if 'client_id' in event['headers']:
-        db = database(event['headers']['client_id'])
-        body = db.get_client()
+    if 'headers' in event:
+        if 'client_id' in event['headers']:
+            db = database(event['headers']['client_id'])
+            body = db.get_client()
+        else:
+            db = database()
+            body = db.get_clients()
     else:
-        db = database()
-        body = db.get_clients()
+        if 'client_id' in event:
+            db = database(event['client_id'])
+            body = db.get_client()
+        else:
+            db = database()
+            body = db.get_clients()
+
 
     response = {
         "statusCode": 200,
@@ -25,12 +34,21 @@ def clients(event, context):
 
 def cameras(event, context):
 
-    if 'camera_id' in event['headers']:
-        db = database(event['headers']['camera_id'])
-        body = db.get_camera()
+    if 'headers' in event:
+        if 'camera_id' in event['headers']:
+            db = database(event['headers']['camera_id'])
+            body = db.get_camera()
+        else:
+            db = database()
+            body = db.get_cameras()
     else:
-        db = database()
-        body = db.get_cameras()
+        if 'camera_id' in event:
+            db = database(event['camera_id'])
+            body = db.get_camera()
+        else:
+            db = database()
+            body = db.get_cameras()
+
 
     response = {
         "statusCode": 200,
@@ -46,54 +64,94 @@ def cameras(event, context):
 
 def client_cameras(event, context):
 
-    if 'client_id' in event['headers']:
-        db = database(event['headers']['client_id'])
-        body = db.get_client_camera()
+    if 'headers' in event:
+        if 'client_id' in event['headers']:
+            db = database(event['headers']['client_id'])
+            body = db.get_client_camera()
+        else:
+            body = "client_id not present in headers"
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(body),
+            "headers": {"Access-Control-Allow-Origin": "*",  # Required for CORS support to work
+                        "Access-Control-Allow-Credentials": True  # Required for cookies, authorization headers with HTTPS
+                        },
+        }
     else:
-        body = "client_id not present in headers"
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body),
-        "headers": {"Access-Control-Allow-Origin": "*",  # Required for CORS support to work
-                    "Access-Control-Allow-Credentials": True  # Required for cookies, authorization headers with HTTPS
-                    },
-    }
+        if 'client_id' in event:
+            db = database(event['client_id'])
+            body = db.get_client_camera()
+        else:
+            body = "client_id not present in headers"
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(body),
+            "headers": {"Access-Control-Allow-Origin": "*",  # Required for CORS support to work
+                        "Access-Control-Allow-Credentials": True  # Required for cookies, authorization headers with HTTPS
+                        },
+        }
 
 
     return response
 
 
 def streams(event, context):
+    if 'headers' in event:
+        if 'camera_id' in event['headers']:
+            db = database(event['headers']['camera_id'])
+            body = db.get_stream()
+        else:
+            body = 'camera_id not present in headers'
 
-    if 'camera_id' in event['headers']:
-        db = database(event['headers']['camera_id'])
-        body = db.get_stream()
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(body),
+            "headers": {"Access-Control-Allow-Origin": "*",  # Required for CORS support to work
+                        "Access-Control-Allow-Credentials": True  # Required for cookies, authorization headers with HTTPS
+                        },
+        }
     else:
-        body = 'camera_id not present in headers'
+        if 'camera_id' in event:
+            db = database(event['camera_id'])
+            body = db.get_stream()
+        else:
+            body = 'camera_id not present in headers'
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body),
-        "headers": {"Access-Control-Allow-Origin": "*",  # Required for CORS support to work
-                    "Access-Control-Allow-Credentials": True  # Required for cookies, authorization headers with HTTPS
-                    },
-    }
-
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(body),
+            "headers": {"Access-Control-Allow-Origin": "*",  # Required for CORS support to work
+                        "Access-Control-Allow-Credentials": True
+                        # Required for cookies, authorization headers with HTTPS
+                        },
+        }
 
     return response
 
 def stream_details(event, context):
-
-    if 'camera_id' in event['headers']:
-        db = database(event['headers']['camera_id'])
-        if 'start_time' in event['headers'] and 'end_time' in event['headers']:
-            stime = event['headers']['start_time']
-            etime = event['headers']['end_time']
-            body = db.get_stream_details_by_time(stime,etime)
+    if 'headers' in event:
+        if 'camera_id' in event['headers']:
+            db = database(event['headers']['camera_id'])
+            if 'start_time' in event['headers'] and 'end_time' in event['headers']:
+                stime = event['headers']['start_time']
+                etime = event['headers']['end_time']
+                body = db.get_stream_details_by_time(stime,etime)
+            else:
+                body = db.get_stream_details()
         else:
-            body = db.get_stream_details()
+            body = 'camera_id not present in headers'
     else:
-        body = 'camera_id not present in headers'
+        if 'camera_id' in event:
+            db = database(event['camera_id'])
+            if 'start_time' in event and 'end_time' in event:
+                stime = event['start_time']
+                etime = event['end_time']
+                body = db.get_stream_details_by_time(stime,etime)
+            else:
+                body = db.get_stream_details()
+        else:
+            body = 'camera_id not present in headers'
+
 
     response = {
         "statusCode": 200,
@@ -107,21 +165,37 @@ def stream_details(event, context):
     return response
 
 def stream_metadata(event, context):
-
-    if 'camera_id' in event['headers']:
-        db = database(event['headers']['camera_id'])
-        if 'start_time' in event['headers'] and 'end_time' in event['headers']:
-            stime = event['headers']['start_time']
-            etime = event['headers']['end_time']
-            if 'label' in event['headers']:
-                label = event['headers']['label']
-                body = db.get_stream_metadata_by_time(stime, etime,label)
+    if 'headers' in event:
+        if 'camera_id' in event['headers']:
+            db = database(event['headers']['camera_id'])
+            if 'start_time' in event['headers'] and 'end_time' in event['headers']:
+                stime = event['headers']['start_time']
+                etime = event['headers']['end_time']
+                if 'label' in event['headers']:
+                    label = event['headers']['label']
+                    body = db.get_stream_metadata_by_time(stime, etime,label)
+                else:
+                    body = 'label not present in headers'
             else:
-                body = 'label not present in headers'
+                body = 'start_time or end_time not present in headers'
         else:
-            body = 'start_time or end_time not present in headers'
+            body = 'camera_id not present in headers'
     else:
-        body = 'camera_id not present in headers'
+        if 'camera_id' in event:
+            db = database(event['camera_id'])
+            if 'start_time' in event and 'end_time' in event:
+                stime = event['start_time']
+                etime = event['end_time']
+                if 'label' in event:
+                    label = event['label']
+                    body = db.get_stream_metadata_by_time(stime, etime,label)
+                else:
+                    body = 'label not present in headers'
+            else:
+                body = 'start_time or end_time not present in headers'
+        else:
+            body = 'camera_id not present in headers'
+
 
     response = {
         "statusCode": 200,
